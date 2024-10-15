4165ffe3dc1309863917256150386664cde6b395
22UCS097 <ravikumar628763@gmail.com>
2024-10-15 09:47:42 +0530

    created user model

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';//we cannto use .env in backend directally
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected to mongoose");
+})
+.catch((err)=>{
+    console.log(err);
+})
 const app=express();
 
 app.listen(3000,()=>{
